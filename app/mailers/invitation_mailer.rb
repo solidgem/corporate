5551f8319f8fbe2d3fb1c393c82479cb00048441
Grandman <root@grandman73.ru>
2015-06-23 11:37:53 +0300

    locales and errors
    locales and errors
    
    locales and errors

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,6 +1,6 @@
 class InvitationMailer < ApplicationMailer
   def invitation_email(user)
     @user = user
-    mail(to: user.email, subject: t(:invite))
+    mail(to: user.email, subject: t(:mail_invite_subject))
   end
 end
