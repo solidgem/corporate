5f60e030b8d1d10f953d69c64901328c8977a425
Grandman <root@grandman73.ru>
2015-06-24 11:10:10 +0300

    submit buttons, comments in bootstrap styles

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Corporate
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.action_mailer.default_url_options = { host: Figaro.env.root_url }
   end
 end
