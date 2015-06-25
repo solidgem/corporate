6510d17f3fb5fc03da4550269529fcea9657e082
Grandman <root@grandman73.ru>
2015-06-25 11:36:00 +0300

    Fixed heroku deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.find_or_initialize_by mail: 'admin@mail.ru'
+user = User.find_or_initialize_by email: 'admin@mail.ru'
 user.update! password: '123456'
