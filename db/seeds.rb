7c968db0ba5edaa79bf3156e23c5860185883540
Grandman <root@grandman73.ru>
2015-06-29 11:29:52 +0300

    delete profile, fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create_with(password: '123456').find_or_create_by!(email: 'admin@mail.ru')
+User.create_with(password: '123456', name: "Тестовый Пользователь").find_or_create_by!(email: 'admin@mail.ru')
