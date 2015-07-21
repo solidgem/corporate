bc987b2bb58b7a90dcbe0bf6dd137f9d837463ec
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2015-07-21 15:27:40 +0300

    fix text

--- app/assets/javascripts/selectpicker.js
+++ app/assets/javascripts/selectpicker.js
@@ -1,7 +1,8 @@
 //= require bootstrap-select
 
 $.fn.selectpicker.defaults = {
-    noneSelectedText: 'Ничего не выбрано',
+    //noneSelectedText: 'Ничего не выбрано',
+    noneSelectedText: '',
     noneResultsText: 'Совпадений не найдено {0}',
     countSelectedText: 'Выбрано {0} из {1}',
     maxOptionsText: ['Достигнут предел ({n} {var} максимум)', 'Достигнут предел в группе ({n} {var} максимум)', ['items', 'item']],
