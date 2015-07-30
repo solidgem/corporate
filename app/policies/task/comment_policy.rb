50705dd3fc85b8314101e5ceb032d28c09a1c646
Grandman <root@grandman73.ru>
2015-07-30 15:48:18 +0300

    policy for new and create comment

--- app/policies/task/comment_policy.rb
+++ app/policies/task/comment_policy.rb
@@ -1,6 +1,16 @@
 class Task::CommentPolicy < ApplicationPolicy
-  delegate :index?, :show?, :new?, :create?, :update?, :edit?, :destroy?, to: :task_policy
+  delegate :index?, :show?, :update?, :edit?, :destroy?, to: :task_policy
 
+  def new?
+    return false if user.guest?
+    return true if user.top_manager?
+    return true if record.task.member? user
+    false
+  end
+
+  def create?
+    new?
+  end
   private
 
   def task_policy
