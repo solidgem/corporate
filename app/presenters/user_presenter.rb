345ca5110a359ba37206164df0d9ac68842c9280
Grandman <root@grandman73.ru>
2015-07-31 16:51:42 +0300

    add presenters for other user attributes

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -4,13 +4,23 @@ class UserPresenter < BasePresenter
     h.image_tag model.avatar.small.url, AvatarUploader::SMALL_DIMENSIONS
   end
 
+  def contacts
+    return unless model.contacts?
+    h.markdown model.contacts
+  end
+
   def requisites
     return unless model.requisites?
     h.markdown model.requisites
   end
 
-  def position
-    return unless model.position?
-    h.markdown model.position
+  def hour_rate
+    return unless model.hour_rate?
+    model.hour_rate.to_s(:currency)
+  end
+
+  def external_hour_rate
+    return unless model.external_hour_rate?
+    model.external_hour_rate.to_s(:currency)
   end
 end
