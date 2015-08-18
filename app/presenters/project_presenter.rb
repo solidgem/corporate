ef63704549a345e883f149b75de4710df9011ef3
Grandman <root@grandman73.ru>
2015-08-18 17:20:08 +0300

    add project policy

--- app/presenters/project_presenter.rb
+++ app/presenters/project_presenter.rb
@@ -1,4 +1,9 @@
 class ProjectPresenter < BasePresenter
+  def initialize(model, view_context)
+    access_presenter = AccessPresenter.new model, view_context
+    super access_presenter, view_context
+  end
+
   def title
     h.link_to_if h.policy(model).show?, model.title, model
   end
@@ -8,6 +13,7 @@ class ProjectPresenter < BasePresenter
   end
 
   def cost
+    return unless model.cost
     model.cost.to_s(:currency)
   end
 end
