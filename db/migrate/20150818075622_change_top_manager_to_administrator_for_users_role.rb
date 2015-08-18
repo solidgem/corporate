7559764cd8a18214eea4a331f54e7174dcc3bd65
Grandman <root@grandman73.ru>
2015-08-18 11:41:56 +0300

    fix migration

--- db/migrate/20150818075622_change_top_manager_to_administrator_for_users_role.rb
+++ db/migrate/20150818075622_change_top_manager_to_administrator_for_users_role.rb
@@ -1,9 +1,12 @@
 class ChangeTopManagerToAdministratorForUsersRole < ActiveRecord::Migration
+  class User < ActiveRecord::Base
+  end
+
   def up
     User.find_each do |user|
       if user.role == 'top_manager'
         user.role = 'administrator'
-        user.save
+        user.save!
       end
     end
   end
