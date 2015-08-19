63a56cef101a6582c09c99fb953f78e0954c3de6
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2015-08-19 10:05:00 +0300

    fix pundit form builder

--- lib/pundit_form_builder.rb
+++ lib/pundit_form_builder.rb
@@ -1,6 +1,14 @@
 class PunditFormBuilder < SimpleForm::FormBuilder
   def input(attribute_name, options = {}, &block)
-    return unless template.policy(object).permitted_attributes.include? attribute_name
+    return unless show_attribute? attribute_name
     super(attribute_name, options, &block)
+  end
+
+
+  def show_attribute?(attr_name)
+    permitted_attributes = template.policy(object).permitted_attributes
+    return true if permitted_attributes.include? attr_name
+    return true if permitted_attributes.last.kind_of?(Hash) && permitted_attributes.last.key?(attr_name)
+    false
   end
 end
