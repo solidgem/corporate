f0bcf13c6ff8e40019b723fc6c39ed1e75660eb6
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2015-08-24 15:43:27 +0300

    Update project_repository.rb

--- app/repositories/project_repository.rb
+++ app/repositories/project_repository.rb
@@ -2,8 +2,9 @@ module ProjectRepository
   extend ActiveSupport::Concern
 
   included do
-    # Код дублируется из task_repository, т.к. не удалось сделать merge (http://api.rubyonrails.org/classes/ActiveRecord/SpawnMethods.html#method-i-merge)
-    # с joins, используя гем squeel (https://github.com/activerecord-hackery/squeel).
+    # Код дублируется из task_repository, т.к. не удалось сделать merge с правильным join. 
+    # http://api.rubyonrails.org/classes/ActiveRecord/SpawnMethods.html#method-i-merge
+    # https://github.com/activerecord-hackery/squeel
         scope :for_user, ->(user) { joins{ tasks.task_participations }
                                 .where{ (tasks.creator_id == my{user.id}) \
                                       | (tasks.responsible_user_id == my{user.id}) \
