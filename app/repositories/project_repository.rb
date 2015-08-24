b4a9a92318573440d2fbe1c428e03cdad4bca3f5
Grandman <root@grandman73.ru>
2015-08-24 15:23:40 +0300

    add comment for project scope for_user

--- app/repositories/project_repository.rb
+++ app/repositories/project_repository.rb
@@ -2,11 +2,13 @@ module ProjectRepository
   extend ActiveSupport::Concern
 
   included do
-    scope :for_user, ->(user)   { joins{ tasks.task_participations }
-                                 .where{ (tasks.creator_id == my{user.id}) \
-                                       | (tasks.responsible_user_id == my{user.id}) \
-                                       | (task_participations.user_id == my{user.id}) }
-                                 .uniq }
+    # Код дублируется из task_repository, т.к. не удалось сделать merge (http://api.rubyonrails.org/classes/ActiveRecord/SpawnMethods.html#method-i-merge)
+    # с joins, используя гем squeel (https://github.com/activerecord-hackery/squeel).
+        scope :for_user, ->(user) { joins{ tasks.task_participations }
+                                .where{ (tasks.creator_id == my{user.id}) \
+                                      | (tasks.responsible_user_id == my{user.id}) \
+                                      | (task_participations.user_id == my{user.id}) }
+                                .uniq }
     scope :for_worker, ->(worker) { for_user(worker) }
   end
 end
