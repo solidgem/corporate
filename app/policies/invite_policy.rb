ca116469db4d0c012d1868d8517c3a68ebac684d
Grandman <root@grandman73.ru>
2015-08-26 09:42:06 +0300

    permitted roles for invite

--- app/policies/invite_policy.rb
+++ app/policies/invite_policy.rb
@@ -1,5 +1,10 @@
 class InvitePolicy < ApplicationPolicy
   def create?
-    user.administrator?
+    user.administrator? || user.manager?
+  end
+
+  def permitted_roles
+    return [[User.role.worker.text, :worker]] if user.manager?
+    User.role.options if user.administrator?
   end
 end
