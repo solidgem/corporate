bcaa071d8c670314fdc0d99a940bdc501c35643d
Grandman <root@grandman73.ru>
2015-08-28 15:13:44 +0300

    fix

--- spec/factories/task_factory.rb
+++ spec/factories/task_factory.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :task do
     title
     description
-    association :creator, factory: :administrator
+    association :creator, factory: :user
     competence { Task.competence.values.sample }
   end
 end
