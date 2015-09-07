5a2ea84d5cd5b3bdd832d6776b3fda95b2f13f4f
Grandman <root@grandman73.ru>
2015-09-07 14:58:48 +0300

    revert changes for users_spec

--- spec/requests/web/users_spec.rb
+++ spec/requests/web/users_spec.rb
@@ -2,22 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'users', type: :request do
   let(:user) { create :user }
+  before(:each){ sign_in_user user }
 
   context 'index' do
     it 'render with 200 status' do
-      sign_in_user user
       get "/users"
       expect(response).to be_success
-    end
-    it 'redirect if not logged' do
-      get "/users"
-      expect(response).to be_redirect
     end
   end
 
   context 'show' do
     it 'render with 200 status' do
-      sign_in_user user
       get "/users/#{user.id}"
       expect(response).to be_success
     end
@@ -25,7 +20,6 @@ RSpec.describe 'users', type: :request do
 
   context 'edit' do
     it 'render with 200 status' do
-      sign_in_user user
       get "/users/#{user.id}/edit"
       expect(response).to be_success
     end
@@ -35,7 +29,6 @@ RSpec.describe 'users', type: :request do
     let(:email) { generate :email }
 
     it 'success' do
-      sign_in_user user
       patch "/users/#{user.id}", user: { email: email }
       expect(response).to be_redirect
     end
