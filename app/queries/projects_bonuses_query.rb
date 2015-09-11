06b862c45a16d5a5693ec43a470013fc331dd93e
Grandman <root@grandman73.ru>
2015-09-11 11:09:52 +0300

    refactor

--- app/queries/projects_bonuses_query.rb
+++ app/queries/projects_bonuses_query.rb
@@ -2,11 +2,11 @@ module ProjectsBonusesQuery
   module_function
 
   def perform(user, start_date, end_date)
-    deadline_bonus_coefficients = configs[:deadline_bonus_coefficients]
+    deadline_bonus_coefficients = PmBonusCoefficients.deadline_bonus_coefficients
     response = ActiveRecord::Base.connection.exec_query <<-SQL
       SELECT projects.id,
-             (projects.cost - SUM(COALESCE(task_comments.elapsed_time,0) / 3600.0 * COALESCE(users.external_hour_rate,0))) *
-             (#{configs[:base_percent].to_f} +
+             (projects.cost - SUM(COALESCE(task_comments.elapsed_time, 0) / 3600.0 * COALESCE(users.external_hour_rate, 0))) *
+             (#{PmBonusCoefficients.base_percent} +
               CASE
                 WHEN projects.finished_at < projects.deadline
                   THEN #{deadline_bonus_coefficients[:pm_ahead_of_schedule]}
@@ -21,14 +21,17 @@ module ProjectsBonusesQuery
                END)
               /
               100 +
-              (COALESCE(projects.complaints,0) * #{configs[:complaints].to_f} +
-               COALESCE(projects.critical_complaints,0) * #{configs[:critical_complaints].to_f} +
-               (CASE WHEN projects.letters_of_thanks THEN #{configs[:letters_of_thanks].to_f} ELSE 0 END)) as bonus
+              (COALESCE(projects.complaints, 0) * #{PmBonusCoefficients.complaints} +
+               COALESCE(projects.critical_complaints, 0) * #{PmBonusCoefficients.critical_complaints} +
+               (CASE WHEN projects.letters_of_thanks THEN #{PmBonusCoefficients.letters_of_thanks} ELSE 0 END)) as bonus
       FROM projects
       LEFT OUTER JOIN tasks ON tasks.project_id = projects.id
       LEFT OUTER JOIN task_comments ON task_comments.task_id = tasks.id
       LEFT OUTER JOIN users ON users.id = task_comments.user_id
-      WHERE projects.responsible_user_id = #{user.id} AND projects.finished_at < '#{end_date}' AND projects.finished_at > '#{start_date}'
+      WHERE projects.responsible_user_id = #{user.id}
+        AND projects.status = 'finished'
+        AND projects.finished_at < '#{end_date}'
+        AND projects.finished_at > '#{start_date}'
       GROUP BY projects.id
     SQL
     bonuses = response.rows.map{ |id, bonus| [id.to_i, bonus.to_f] }.to_h
@@ -39,9 +42,5 @@ module ProjectsBonusesQuery
           bonus: bonuses.fetch(project.id, 0.0)
       }
     end
-  end
-
-  def configs
-    CustomConfigs.by_type(:pm_bonus_coefficients)
   end
 end
--- lib/custom_configs.rb
+++
@@ -1,6 +0,0 @@
-module CustomConfigs
-  def self.by_type(type)
-    @default_formulas_conf ||= ::YAML.load(File.read("config/default_params_for_formulas.yaml"))
-    @default_formulas_conf[type]
-  end
-end
