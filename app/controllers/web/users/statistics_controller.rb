2d8d8a4928c05634aa40fcb7b54d0ded3941b588
Grandman <root@grandman73.ru>
2015-10-14 10:44:19 +0300

    fix template

--- app/controllers/web/users/statistics_controller.rb
+++ app/controllers/web/users/statistics_controller.rb
@@ -6,5 +6,13 @@ class Web::Users::StatisticsController < Web::Users::ApplicationController
     @statistics = UserStatisticsQuery.perform resource_user,
                                               @filter_form.start_date,
                                               @filter_form.end_date
+    start_date = @filter_form.start_date.to_datetime.strftime('%d.%m.%Y')
+    end_date = @filter_form.end_date.to_datetime.strftime('%d.%m.%Y')
+    filename = "#{resource_user}-#{start_date}-#{end_date}"
+    respond_with do |format|
+      format.xlsx {
+        response.headers['Content-Disposition'] = "attachment; filename='#{filename}.xlsx'"
+      }
+    end
   end
 end
