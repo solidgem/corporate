c1b303edadf6e4742f04d6923fa3846546b68de9
Grandman <root@grandman73.ru>
2015-10-15 15:04:33 +0300

    refactor

--- app/controllers/web/users/statistics_controller.rb
+++ app/controllers/web/users/statistics_controller.rb
@@ -6,13 +6,13 @@ class Web::Users::StatisticsController < Web::Users::ApplicationController
     @statistics = UserStatisticsQuery.perform resource_user,
                                               @date_form.start_date,
                                               @date_form.end_date
-    start_date = @date_form.start_date.to_datetime.strftime('%d.%m.%Y')
-    end_date = @date_form.end_date.to_datetime.strftime('%d.%m.%Y')
-    filename = "#{resource_user}-#{start_date}-#{end_date}"
     respond_with do |format|
-      format.xlsx {
+      format.xlsx do
+        start_date = @date_form.start_date.to_datetime.strftime('%d.%m.%Y')
+        end_date = @date_form.end_date.to_datetime.strftime('%d.%m.%Y')
+        filename = "#{resource_user}-#{start_date}-#{end_date}"
         response.headers['Content-Disposition'] = "attachment; filename='#{filename}.xlsx'"
-      }
+      end
     end
   end
 end
