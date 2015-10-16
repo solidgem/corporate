2c0ce5c360b125e91ab8c6e9ccc9603222a82883
Grandman <root@grandman73.ru>
2015-10-16 12:44:06 +0300

    add xlsx for pm_bonus, fix n+1 for pm_bonus show

--- app/controllers/web/users/pm_bonus_controller.rb
+++ app/controllers/web/users/pm_bonus_controller.rb
@@ -4,5 +4,13 @@ class Web::Users::PmBonusController < Web::Users::ApplicationController
     add_breadcrumb
     @date_form = User::DateForm.new params[:user_date_form]
     @bonuses = ProjectsBonusesQuery.perform(resource_user, @date_form.start_date, @date_form.end_date)
+    respond_with do |format|
+      format.xlsx do
+        start_date = @date_form.start_date.to_datetime.strftime('%d.%m.%Y')
+        end_date = @date_form.end_date.to_datetime.strftime('%d.%m.%Y')
+        filename = "#{resource_user}-#{start_date}-#{end_date}"
+        response.headers['Content-Disposition'] = "attachment; filename='#{filename}.xlsx'"
+      end
+    end
   end
 end
