b23aea3ae2e1f792eadb3a8a9e4ba7a82440adad
Grandman <root@grandman73.ru>
2015-10-20 17:54:41 +0300

    fix access presenter

--- app/presenters/access_presenter.rb
+++ app/presenters/access_presenter.rb
@@ -1,10 +1,16 @@
 class AccessPresenter < BasePresenter
   def initialize(model, view_context)
     super
-    not_access_attributes = model.attribute_names.map(&:to_sym) - view_context.policy(model).readable_attributes
+    not_access_attributes = model.attribute_names.map(&:to_s) - view_context.policy(model).readable_attributes.map(&:to_s)
     not_access_attributes.each do |attribute|
       define_singleton_method(attribute) { nil }
       define_singleton_method("#{attribute}?") { false }
+
+      association_match = attribute.match(/(?<association>.*)_id/)
+      if association_match
+        association_name = association_match[:association]
+        define_singleton_method(association_name) { nil }
+      end
     end
   end
 end
