59c9a270cffa96ad7666da0540e44fddfe40182a
Grandman <root@grandman73.ru>
2015-10-26 16:30:20 +0300

    fix test

--- app/presenters/access_presenter.rb
+++ app/presenters/access_presenter.rb
@@ -3,8 +3,12 @@ class AccessPresenter < BasePresenter
     super
     model.attribute_names.each do |attribute|
       unless view_context.policy(model).readable_attributes.readable? attribute
-        define_singleton_method(attribute) { nil }
-        define_singleton_method("#{attribute}?") { false }
+        attr_name = attribute.sub(/_id\z/, '')
+        define_singleton_method(attr_name) { nil }
+        define_singleton_method("#{attr_name}?") { false }
+
+        define_singleton_method("#{attr_name}_id") { nil }
+        define_singleton_method("#{attr_name}_id?") { false }
       end
     end
   end
--- spec/factories/project_factory.rb
+++ spec/factories/project_factory.rb
@@ -4,5 +4,6 @@ FactoryGirl.define do
     deadline { generate :datetime }
     cost { generate :integer }
     responsible_user_id { create(:user).id }
+    counterparty_id { create(:counterparty).id }
   end
 end
