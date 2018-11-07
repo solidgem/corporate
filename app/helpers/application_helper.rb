0f092f3edb70136c82a584605adb4d558285dd36
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2018-11-07 21:19:47 +0400

    date correction

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -70,7 +70,7 @@ module ApplicationHelper
 
   def present(model)
     return if model.blank?
-    klass = "#{model.class}Presenter".constantize
+    klass = "#{model.model_name}Presenter".constantize
     presenter = klass.new(model, self)
     yield(presenter) if block_given?
     presenter
